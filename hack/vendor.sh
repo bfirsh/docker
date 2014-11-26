de4a0d9602093c637f2ec314a7e7af8b26c30375
Ruslan Gabitov <hope2believein@gmail.com>
2014-11-25 17:06:28 -0800

    Fixed Remove() method in Azure host
    Fixed Remove() method in Azure host
    
    Signed-off-by: Ruslan Gabitov <ruslan.gabitov@live.com>

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -63,7 +63,7 @@ clone hg code.google.com/p/goauth2 afe77d958c70
 
 clone git github.com/mitchellh/go-homedir 7d2d8c8a4e078ce3c58736ab521a40b37a504c52
 
-clone git github.com/MSOpenTech/azure-sdk-for-go 814812a21a49da38c5cc808ec0958277e33c81d3
+clone git github.com/MSOpenTech/azure-sdk-for-go 515f3ec74ce6a5b31e934cefae997c97bd0a1b1e
 
 # get Go tip's archive/tar, for xattr support and improved performance
 # TODO after Go 1.4 drops, bump our minimum supported version and drop this vendored dep
